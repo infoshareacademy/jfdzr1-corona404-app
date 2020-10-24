0992f782a7e3829573a0821e021466229c86dc6c
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-10-24 20:46:32 +0200

    Routing zepsuty but fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import Dashboard from './dashboard/dashboard'
-import {Route, Switch} from "react-router-dom";
-
-import Cards from './products/productCards/Cards';
-
-import Cart from './cart/cart';
 import MiniDrawer from './navigation/navigation'
 
 
 function App() {
 
   return (
-    <Switch>
-      <Route path="/dashboard">
-        <Dashboard/>
-      </Route>
-      <Route path="/products">
-        <Cards/>
-      </Route>
-      <Route path="/cart">
-        <Cart/>
-      </Route>
-    </Switch>
     <MiniDrawer/>
   );
 }
