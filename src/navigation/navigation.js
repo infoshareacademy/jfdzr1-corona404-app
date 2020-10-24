41d9a70734b3201ed79477e9afa74adce8b09862
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-10-24 11:46:29 +0200

    Update navigation.js

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -103,6 +103,7 @@ export default function MiniDrawer() {
       <CssBaseline />
       <AppBar
         position="fixed"
+        color="secondary"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
