97ed6d36f99fa21cf0f6ce40cd50f443351119c6
tomasz-makarewicz <64966256+tomasz-makarewicz@users.noreply.github.com>
2020-10-25 19:27:58 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import Navigation from './navigation/navigation'
+         import React from "react"
+         import Dashboard from './dashboard/dashboard'
+         import Cart from './cart/cart'
+         import Cards from './products/productCards/Cards'
+         import {Switch, Route, BrowserRouter} from 'react-router-dom'
+         import Navigation from './navigation/navigation'
 
 
 function App() {
 
   return (
+    <BrowserRouter>
     <Navigation/>
-  );
-}
+    <Switch>
+    <Route exact path="/">
+      <Dashboard/>
+     </Route>
+     <Route path="/products">
+      <Cards/>
+     </Route>
+     <Route path="/cart">
+      <Cart/>
+     </Route>
+     <Route path="/form">
+     </Route>
+   </Switch>
+   </BrowserRouter>
+   );
+ }
+
+ 
 
 export default App;
