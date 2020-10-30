ec5b9ea80ca9d7b0ab33fe73272d4cdb85efbab3
maciej-pilarczyk <maciekpilar1@gmail.com>
2020-10-30 14:07:39 +0100

    added ProductModal component

---
+++ src/products/productCards/ProdctModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+class ProductModal extends React.Component {
+    state= {
+        name: '',
+        description: '',
+        image: '',
+        category: '',
+        longDescription: ''
+
+    }
+}
