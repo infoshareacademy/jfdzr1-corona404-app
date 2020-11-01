f68badeca4122ba2d51ce71bd6b929b3392d4342
Mateusz Adamczyk <mateusz.adamczyk.966@gmail.com>
2020-11-02 00:49:25 +0100

    Change picture in final section

--- src/form/FormSuccess.js
+++ src/form/FormSuccess.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Form.css';
-import Congrats from './images/thumbup.svg'
+import Success from './images/success.svg'
 const FormSuccess = () => {
   return (
     <div className='form-content-right'>
       <span className='close-btn'><a href="/" >x</a></span>
       <h1 className='form-success'>Produkt został dodany!</h1>
-      <img className='form-img-2' src={Congrats} alt='success-image' />
+      <img className='form-img-2' src={Success} alt='success-image' />
     </div>
   );
 };
